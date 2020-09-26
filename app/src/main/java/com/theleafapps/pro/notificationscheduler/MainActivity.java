3d38f6fdbf83c441695db95f0999ecb981c61fa4
Abhijeet Singh <redefinedabhi2@gmail.com>
2020-09-27 04:23:48 +0530

    Notification Scheduler working

--- app/src/main/java/com/theleafapps/pro/notificationscheduler/MainActivity.java
+++ app/src/main/java/com/theleafapps/pro/notificationscheduler/MainActivity.java
@@ -1,14 +1,63 @@
 package com.theleafapps.pro.notificationscheduler;
 
+import android.app.job.JobInfo;
+import android.app.job.JobScheduler;
+import android.content.ComponentName;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.RadioGroup;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class MainActivity extends AppCompatActivity {
+
+    private static final int JOB_ID = 0;
+    private JobScheduler mScheduler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+
+    public void scheduleJob(View view) {
+        int selectedNetworkOption = JobInfo.NETWORK_TYPE_NONE;
+        RadioGroup networkOptions = findViewById(R.id.networkOptions);
+
+        mScheduler = (JobScheduler) getSystemService(JOB_SCHEDULER_SERVICE);
+        int selectedNetworkID = networkOptions.getCheckedRadioButtonId();
+
+        switch (selectedNetworkID) {
+            case R.id.noNetwork:
+                selectedNetworkOption = JobInfo.NETWORK_TYPE_NONE;
+                break;
+            case R.id.anyNetwork:
+                selectedNetworkOption = JobInfo.NETWORK_TYPE_ANY;
+                break;
+            case R.id.wifiNetwork:
+                selectedNetworkOption = JobInfo.NETWORK_TYPE_UNMETERED;
+                break;
+        }
+
+        ComponentName serviceName = new ComponentName(getPackageName(), NotificationJobService.class.getName());
+        JobInfo.Builder builder = new JobInfo.Builder(JOB_ID, serviceName);
+        builder.setRequiredNetworkType(selectedNetworkOption);
+
+        JobInfo myJobInfo = builder.build();
+        mScheduler.schedule(myJobInfo);
+
+        Toast.makeText(this, "Job Scheduled, job will run when " +
+                "the constraints are met.", Toast.LENGTH_SHORT).show();
+
+    }
+
+    public void cancelJobs(View view) {
+        if (mScheduler!=null){
+            mScheduler.cancelAll();
+            mScheduler = null;
+            Toast.makeText(this, "Jobs cancelled", Toast.LENGTH_SHORT).show();
+        }
     }
 }
